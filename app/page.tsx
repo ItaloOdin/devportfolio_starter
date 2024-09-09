fe9c0d49e1230eb8a2e5efa942b8e8bf9c9e510e
ItaloOdin <italootsuka416@gmail.com>
2024-09-08 22:39:43 -0400

    atualizações e criaçaõ da navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Home from "@/components/Home/Home";
 import React from "react";
 
 const HomePage = () => {
-  return <div className="text-5xl">WEBDEV WARRIORS</div>;
+  return <div className="overflow">
+    <Home />
+  </div>;
 };
 
 export default HomePage;
